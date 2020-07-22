5692bc2e8586747a3ef370a77b454e4e8c55636e
user-1 <user-1@MacBook-Pro.local>
2020-07-22 16:35:28 -0600

    finished app 3 using solution

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -2,25 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filterString: "",
+      authors: ["Austen", "Dickens", "Bronte", "Gaskell"]
+    }
+  }
+
+  handleChange(filter){
+    this.setState({
+      filterString: filter
+    })
+  }
+
+  render(){
+    let authorsToDisplay = this.state.authors.filter((element, index) => {
+      return element.includes(this.state.filterString)
+      }).map((element, index) => {
+        return <h2 key={index}>{element}</h2>
+    })
+
+    return (
+      <div className="App">
+        <input type="text" onChange={e => this.handleChange(e.target.value)}/>{authorsToDisplay}
+      </div>
+    )
+  }
 }
 
 export default App;
+
+//Notes: You can chain array or string methods. Filter before others, usually. Watch the spacing between the .(method) though. If it's out of place, it doesn't chain correctly.
