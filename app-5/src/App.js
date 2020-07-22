9769ac296b031ffe52dd30d75dd9f03b333f5c13
user-1 <user-1@MacBook-Pro.local>
2020-07-22 17:41:34 -0600

    finished app 5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './components/Image'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Image url={"https://i.pinimg.com/originals/36/45/68/3645685eb57fb5fe67f95f1f2494aa4e.jpg"}/>
+      </div>
+    );
+  }  
 }
 
 export default App;
---
+++ app-5/src/components/Image.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Image extends React.Component {
+    render(){
+        return (
+            <div>
+                <img src={this.props.url}/>
+                <div>Error 414</div>
+            </div>
+        )
+    }   
+}    
+
+
+
+
+
+
+
+
+export default Image
