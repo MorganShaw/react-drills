c6a2a58dba00cabf646375cc390b8139a1ee862a
user-1 <user-1@MacBook-Pro.local>
2020-07-22 15:05:02 -0600

    finished app 2 using solution bc keys new idea

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      books: ["Persuasion", "Northanger Abbey", "Getting to Yes", "Aimless Love"]
+    }
+  }
+
+  render(){
+    let booksToDisplay = this.state.books.map((element, index) => {
+      return <h2 key={index}>{element}</h2>;
+    });
+    return(
+      <div className="App">{booksToDisplay}
+      </div>
+    )
+  }
 }
 
 export default App;
