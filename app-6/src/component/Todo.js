0277f401b845caa93c728e5f301394a14dea9f76
user-1 <user-1@MacBook-Pro.local>
2020-07-22 22:41:26 -0600

    finished app 6

---
+++ app-6/src/component/Todo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class ToDo extends React.Component {
+    render(){
+        return(
+            <p>{this.props.task}
+
+            </p>
+        )  
+    }
+    
+}
+
+export default ToDo
